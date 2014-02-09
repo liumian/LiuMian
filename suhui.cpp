633154422e0fa855efbcb5e763b3959206da12d0
suhui <moliyatihui@gmail.com>
2014-02-09 19:32:57 +0800

    Create suhui.cpp
    to my deer,my lover.

---
+++ suhui.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    std::cout<<
+    "这里是我和我最爱的人的居所。\n
+    我们从零开始搭建我们的小屋。\n
+    一起学习，一起进步。\n
+    最重要的还可以见证我们的爱情。"
+}
